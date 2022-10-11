0a071f88dbb8defa206f0dc17faaca58c14a978a
BryanAafjes <432731@student.fontys.nl>
2022-10-11 20:01:44 +0200

    Added functions to UserService

--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -1,10 +1,39 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn,UpdateDateColumn } from 'typeorm';
 
 @Entity()
+/**
+ * User entity.
+ */
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn("uuid")
-  public id: string;
+  public azure_id: string;
 
   @Column()
-  public name: string;
+  public first_name: string;
+
+  @Column({
+    nullable: true
+  })
+  public insertion: string;
+
+  @Column()
+  public last_name: string;
+
+  @Column()
+  public phone_number: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public birthday: string;
+
+  @Column()
+  public visibility: boolean;
+
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+  public created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+  public updated_at: Date;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import User from './entity/user.entity';
 import GraphHelper from '../graphHelper';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor (private readonly graphHelper:GraphHelper) {}
+  /**
+   * The graph helper for azure.
+   */
+  private _graphHelper : GraphHelper;
+  private _graphClient;
 
-    public async getUsersAsync(): Promise<String> {
-        let _appClient = this.graphHelper.getAppClient();
+  constructor (private readonly graphHelper:GraphHelper, @InjectRepository(User)
+    private userRepository: Repository<User>) {
+    this._graphHelper = graphHelper;
+    this._graphClient = this._graphHelper.getAppClient();
+  }
 
-        let result = _appClient?.api('/users')
-          .select(['id', 'givenName', 'surName', 'mobilePhone', 'mail'])
-          .top(25)
-          .orderby('displayName')
-          .get();
+  /**
+   * Retrieves all users.
+   * @returns {Promise<String>} The users.
+   */
+  public async getUsersAsync(): Promise<String> {
+    let result = this._graphClient?.api('/users')
+      .select(['id', 'givenName', 'surName', 'mobilePhone', 'mail'])
+      .top(25)
+      .orderby('displayName')
+      .get();
 
-        return result;
-    }
+    return result;
+  }
+
+  /**
+   * Signups new user to database.
+   * @param {User} user The user to sign up.
+   * @returns {Promise<boolean>} Whether the sign up was successful.
+   */
+  public async signUpUser(user:User): Promise<boolean> {
+    this.userRepository.save(user);
+  }
+
+  /**
+   * Creates an azure account.
+   * @param {User} user The user to create.
+   */
+  private async createAzureAccount(user:User) {
+    await this._graphClient?.api('/users').;
+  }
 }
